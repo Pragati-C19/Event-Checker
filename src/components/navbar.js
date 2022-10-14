6a3ba6131614db4e1e9d84af3653e9ece7af1378
Pragati Chothe <pragatichothe@gmail.com>
2022-10-14 13:57:33 +0530

    delete unnecessary imports from navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,14 +6,8 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import InputBase from "@mui/material/InputBase";
-import Badge from "@mui/material/Badge";
-import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import MailIcon from "@mui/icons-material/Mail";
-import NotificationsIcon from "@mui/icons-material/Notifications";
 import MoreIcon from "@mui/icons-material/MoreVert";
 import logoImg from "../calender-icon.png";
 
@@ -57,7 +51,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-const PrimarySearchAppBar = () => {
+const Navbar = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
@@ -171,4 +165,4 @@ const PrimarySearchAppBar = () => {
   );
 };
 
-export default PrimarySearchAppBar;
+export default Navbar;
