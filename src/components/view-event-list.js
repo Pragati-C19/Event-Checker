5d503a3d526663930be1fff7e888afbc6b9fc486
Pragati Chothe <pragatichothe@gmail.com>
2022-10-14 14:54:54 +0530

    Table created with dummy data

--- src/components/view-event-list.js
+++ src/components/view-event-list.js
@@ -1,22 +1,78 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(
+  title,
+  eventType,
+  startDate,
+  endDate,
+  viewEvent,
+  editEvent,
+  deleteEvent
+) {
+  return {
+    title,
+    eventType,
+    startDate,
+    endDate,
+    viewEvent,
+    editEvent,
+    deleteEvent,
+  };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData(
+    "Event-1",
+    "Cake",
+    "13th Oct 2022",
+    "13th Oct 2022",
+    "ViewButton",
+    "EditButton",
+    "deleteButton"
+  ),
+  createData(
+    "Event-2",
+    "Bussiness",
+    "13th Oct 2022",
+    "13th Oct 2022",
+    "ViewButton",
+    "EditButton",
+    "deleteButton"
+  ),
+  createData(
+    "Event-3",
+    "Fire",
+    "13th Oct 2022",
+    "13th Oct 2022",
+    "ViewButton",
+    "EditButton",
+    "deleteButton"
+  ),
+  createData(
+    "Event-4",
+    "Book",
+    "13th Oct 2022",
+    "13th Oct 2022",
+    "ViewButton",
+    "EditButton",
+    "deleteButton"
+  ),
+  createData(
+    "Event-5",
+    "Red Flags",
+    "13th Oct 2022",
+    "13th Oct 2022",
+    "ViewButton",
+    "EditButton",
+    "deleteButton"
+  ),
 ];
 
 export default function EventList() {
@@ -25,26 +81,30 @@ export default function EventList() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Event Title</TableCell>
+            <TableCell> Event Type </TableCell>
+            <TableCell>Start Date</TableCell>
+            <TableCell>End Date </TableCell>
+            <TableCell>View</TableCell>
+            <TableCell>Edit</TableCell>
+            <TableCell>Delete</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              key={row.title}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.title}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell>{row.eventType}</TableCell>
+              <TableCell>{row.startDate}</TableCell>
+              <TableCell>{row.endDate}</TableCell>
+              <TableCell>{row.viewEvent}</TableCell>
+              <TableCell>{row.editEvent}</TableCell>
+              <TableCell>{row.deleteEvent}</TableCell>
             </TableRow>
           ))}
         </TableBody>
