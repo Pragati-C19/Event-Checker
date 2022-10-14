0ef4ad954ef41e0d79bd43483dbed1605b5c55ec
Pragati Chothe <pragatichothe@gmail.com>
2022-10-14 17:51:59 +0530

    modal added

--- src/components/add-event.js
+++ src/components/add-event.js
@@ -1,49 +1,88 @@
-import { useState } from 'react'
-import React from 'react'
+import { useState } from "react";
+import * as React from "react";
+import Modal from "@mui/material/Modal";
+import { Box } from "@mui/system";
+import { Typography } from "@mui/material";
+import { Button } from "@mui/material";
+
+//css for modal
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "white",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const AddEvent = ({ onAdd }) => {
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
- 
+  //title and description are input from user
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-  const onSubmit = (e) => {
-    e.preventDefault()
+  //to open and close modal
+  const [modalOpen, setModalOpen] = React.useState(false);
+  const isModalOpen = () => setModalOpen(true);
+  const isModalClose = () => setModalOpen(false);
+
+  //this alert msg is to see wheather the event is added or not
+  const onClick = (e) => {
+    e.preventDefault();
 
     if (!title) {
-      alert('Please add a Event')
-      return
+      alert("Please add a Event");
+      return;
     }
 
-    onAdd({ text: title, day: description})
+    onAdd({ title, description });
 
-    setTitle('')
-    setDescription('')
-  }
+    setTitle("");
+    setDescription("");
+  };
 
   return (
-    <form className='add-event' onSubmit={onSubmit}>
-      <div className='event-control'>
-        <label>Event Title</label>
-        <input
-          type='text'
-          placeholder='Add Event Title'
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-      </div>
-      <div className='event-control'>
-        <label>Description</label>
-        <input
-          type='text'
-          placeholder='Add Event Description'
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-      </div>
+    <div>
+      <Button onClick={isModalOpen}>Open modal</Button>
+      <Modal
+        open={modalOpen}
+        onClose={isModalClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyle}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add New Event
+          </Typography>
+          <div className="add-event" onClick={onClick}>
+            <div className="event-control">
+              <label>Event Title</label>
+              <input
+                type="text"
+                placeholder="Add Event Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div className="event-control">
+              <label>Description</label>
+              <input
+                type="text"
+                placeholder="Add Event Description"
+                value={description}
+                
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
 
-      <input type='submit' value='Save Event' className='btn btn-block' />
-    </form>
-  )
-}
+            <input type="submit" value="Save Event" className="btn btn-block" />
+          </div>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
 
-export default AddEvent
+export default AddEvent;
