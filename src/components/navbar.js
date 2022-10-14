278358366c697b9923311843e15b1c39e03695f3
Pragati Chothe <pragatichothe@gmail.com>
2022-10-14 13:53:56 +0530

    Delete all unnecessary code from navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -97,10 +97,7 @@ const PrimarySearchAppBar = () => {
       }}
       open={isMenuOpen}
       onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
+    ></Menu>
   );
 
   const mobileMenuId = "primary-search-account-menu-mobile";
@@ -119,40 +116,7 @@ const PrimarySearchAppBar = () => {
       }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="error">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton
-          size="large"
-          aria-label="show 17 new notifications"
-          color="inherit"
-        >
-          <Badge badgeContent={17} color="error">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
+    ></Menu>
   );
 
   return (
@@ -165,8 +129,7 @@ const PrimarySearchAppBar = () => {
             aria-label="open drawer"
             sx={{ mr: 2 }}
           >
-            <img src={logoImg} height="40px"
-            width="40px" />
+            <img src={logoImg} height="40px" width="40px" />
           </IconButton>
 
           <Typography
@@ -187,9 +150,7 @@ const PrimarySearchAppBar = () => {
               inputProps={{ "aria-label": "search" }}
             />
           </Search>
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            
-          </Box>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}></Box>
           <Box sx={{ display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
