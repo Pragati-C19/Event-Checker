c69c9eede0bc810b443f8fc43300981026b7e1eb
Pragati Chothe <pragatichothe@gmail.com>
2022-10-14 13:59:03 +0530

    change function name:PrimarySearchAppBar to Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
 import AddEvent from "./components/add-event";
-import PrimarySearchAppBar from "./components/navbar";
+import Navbar from "./components/navbar";
 import BasicTable from "./components/view-event-list";
 
 function App() {
   return (
     <>
-      <PrimarySearchAppBar />
+      <Navbar />
       <BasicTable />
       <AddEvent />
     </>
