cc1e67aea348b82a764e6fc4c32d21a0b78dca50
Pragati Chothe <pragatichothe@gmail.com>
2022-10-17 20:17:14 +0530

    Unique Id for new events

--- src/App.js
+++ src/App.js
@@ -7,82 +7,88 @@ import Events from "./components/events";
 import { useState } from "react";
 
 function App() {
-
   //Array of Event Objects
-  const [events,setEvents]=useState(
-    [
-        {
-          id: 1,
-          title: "Event-1",
-          typeOfEvent: "Cake",
-          startDate: "13th Oct 2022",
-          endDate: "13th Oct 2022",
-          viewEvent: "ViewButton",
-          editEvent: "EditButton",
-          deleteEvent: "deleteButton",
-        },
-        {
-          id: 2,
-          title: "Event-2",
-          typeOfEvent: "Cake",
-          startDate: "13th Oct 2022",
-          endDate: "13th Oct 2022",
-          viewEvent: "ViewButton",
-          editEvent: "EditButton",
-          deleteEvent: "deleteButton",
-        },
-        {
-          id: 3,
-          title: "Event-3",
-          typeOfEvent: "Cake",
-          startDate: "13th Oct 2022",
-          endDate: "13th Oct 2022",
-          viewEvent: "ViewButton",
-          editEvent: "EditButton",
-          deleteEvent: "deleteButton",
-        },
-        {
-          id: 4,
-          title: "Event-4",
-          typeOfEvent: "Cake",
-          startDate: "13th Oct 2022",
-          endDate: "13th Oct 2022",
-          viewEvent: "ViewButton",
-          editEvent: "EditButton",
-          deleteEvent: "deleteButton",
-        },
-        {
-          id: 5,
-          title: "Event-5",
-          typeOfEvent: "Cake",
-          startDate: "13th Oct 2022",
-          endDate: "13th Oct 2022",
-          viewEvent: "ViewButton",
-          editEvent: "EditButton",
-          deleteEvent: "deleteButton",
-        },
-      ]
-)
+  const [events, setEvents] = useState([
+    {
+      id: 1,
+      title: "Event-1",
+      typeOfEvent: "Cake",
+      startDate: "13th Oct 2022",
+      endDate: "13th Oct 2022",
+      viewEvent: "ViewButton",
+      editEvent: "EditButton",
+      deleteEvent: "deleteButton",
+    },
+    {
+      id: 2,
+      title: "Event-2",
+      typeOfEvent: "Cake",
+      startDate: "13th Oct 2022",
+      endDate: "13th Oct 2022",
+      viewEvent: "ViewButton",
+      editEvent: "EditButton",
+      deleteEvent: "deleteButton",
+    },
+    {
+      id: 3,
+      title: "Event-3",
+      typeOfEvent: "Cake",
+      startDate: "13th Oct 2022",
+      endDate: "13th Oct 2022",
+      viewEvent: "ViewButton",
+      editEvent: "EditButton",
+      deleteEvent: "deleteButton",
+    },
+    {
+      id: 4,
+      title: "Event-4",
+      typeOfEvent: "Cake",
+      startDate: "13th Oct 2022",
+      endDate: "13th Oct 2022",
+      viewEvent: "ViewButton",
+      editEvent: "EditButton",
+      deleteEvent: "deleteButton",
+    },
+    {
+      id: 5,
+      title: "Event-5",
+      typeOfEvent: "Cake",
+      startDate: "13th Oct 2022",
+      endDate: "13th Oct 2022",
+      viewEvent: "ViewButton",
+      editEvent: "EditButton",
+      deleteEvent: "deleteButton",
+    },
+  ]);
 
-//Save Event
-const saveEvent=(task)=>{
-  console.log(task)
-}
+  //Save Event
+  const saveEvent = (task) => {
+    //console.log(task)
+    const id = Math.floor(Math.random() * 10000) + 1; //unique Id is able to give for new events after save
+    const newEvent = { id, ...task };
+    setEvents([...events, newEvent]);
+    console.log(id);
+    console.log(newEvent);
+  };
 
-//Delete Event 
-const deleteEvent =(id)=>{
-  //console.log("delete",id)
-  setEvents(events.filter ((task)=>task.id !==id))
-}
+  //Delete Event
+  const deleteEvent = (id) => {
+    //console.log("delete",id)
+    setEvents(events.filter((task) => task.id !== id));
+  };
 
   return (
     <>
       <Navbar />
       <EventList />
-      <AddEvent onSave={saveEvent}/>
+      <AddEvent onSave={saveEvent} />
 
       <h3>Pragati Chothe</h3>
-      {events.length > 0 ? <Events events={events} onDelete={deleteEvent}/> :"No Events to Show"}
+      {events.length > 0 ? (
+        <Events events={events} onDelete={deleteEvent} />
+      ) : (
+        "No Events to Show"
+      )}
     </>
   );
 }
